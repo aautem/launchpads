9d1948dfad74858108f19324d4588b87e22d2f65
Alex Autem <autem.alex@gmail.com>
2017-06-07 15:18:22 -0700

    Complete launch pad map functionality

--- map.js
+++ map.js
@@ -2,7 +2,7 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWF1dGVtIiwiYSI6ImNqM21kNHlzYjAwMGMycW5kbGpvcWRsanQifQ.pgAAHuYJxNuOVaIYGB_WkQ';
 
-// initial map settings
+// initial map setup
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/basic-v9',
@@ -13,34 +13,8 @@ var map = new mapboxgl.Map({
 // add zoom controls to map
 map.addControl(new mapboxgl.NavigationControl());
 
-
-/////////////////////////////
-// MOVE INTO A HELPER FILE //
-/////////////////////////////
-
-var addMapEventHandlers = function() {
-  // Center the map on the coordinates of any clicked symbol from the 'symbols' layer.
-  map.on('click', 'symbols', function (e) {
-    map.flyTo({center: e.features[0].geometry.coordinates, zoom: 8});
-    new mapboxgl.Popup()
-      .setLngLat(e.features[0].geometry.coordinates)
-      .setHTML(e.features[0].properties.description)
-      .addTo(map);
-  });
-
-  // Change the cursor to a pointer when the it enters a feature in the 'symbols' layer.
-  map.on('mouseenter', 'symbols', function () {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-
-  // Change it back to a pointer when it leaves.
-  map.on('mouseleave', 'symbols', function () {
-    map.getCanvas().style.cursor = '';
-  });
-};
-
-
-var createLayer = function(features) {
+map.on('load', function () {
+  // add initial 'symbols' layer to add launch pad locations
   map.addLayer({
     id: 'symbols',
     type: 'symbol',
@@ -48,7 +22,7 @@ var createLayer = function(features) {
       type: 'geojson',
       data: {
         type: 'FeatureCollection',
-        features: features,
+        features: []
       }
     },
     layout: {
@@ -57,123 +31,153 @@ var createLayer = function(features) {
     }
   });
 
-  addMapEventHandlers();
-};
+  addLaunchPadsToMap();
 
-// callback should be anonymous function that calls map.addLayer
-// with "features" prop set to returned features array
-var createFeatures = function(callback) {
-  // AJAX request to get launch pads and locations
-  // ONLY RETURNS 30 LAUNCH PADS AT A TIME
-  // KEEP MAKING API CALLS UNTIL NO RESPONSE
+  // load all rocket information in array of rocket objects
+  getRockets().then(function(rockets) {
+    // add event handlers
+    // handle launch pad click
+    map.on('click', 'symbols', function (e) {
+      // center launchpad location and zoom in
+      map.flyTo({center: e.features[0].geometry.coordinates, zoom: 10});
 
-  axios.get('https://launchlibrary.net/1.2/pad?mode=summary')
-  .then(function (response) {
-    var features = [];
+      // launch pad information
+      var props = e.features[0].properties;
 
-    response.data.pads.forEach(function(pad) {
-      // create 'feature' object
-      var feature = {
-        type: 'Feature',
-        properties: {
-          description: '<h4>' + pad.name + '</h4>'
-        },
-        geometry: {
-          type: 'Point',
-          coordinates: [
-            pad.longitude,
-            pad.latitude
-          ]
+      // rockets associated with this launch pad
+      var launchPadRockets = [];
+
+      rockets.forEach(function(rocket) {
+        if (rocket.defaultPads) {
+          // split defaultPads array at commas
+          var defaultPads = rocket.defaultPads.split(',');
+          defaultPads.forEach(function(id) {
+            if (parseInt(id) === props.id) {
+              launchPadRockets.push(rocket);
+            }
+          });
         }
-      };
-      // push object into 'features' array
-      features.push(feature);
+      });
+
+      var htmlString = '<h4>' + props.name + '</h4>';
+
+      if (launchPadRockets.length) {
+        launchPadRockets.forEach(function(rocket) {
+          htmlString += '<p>' + rocket.name + '</p>';
+        });
+      } else {
+        htmlString += '<p>No rocket information available</p>';
+      }
+
+      // add info popup to map location
+      new mapboxgl.Popup()
+        .setLngLat(e.features[0].geometry.coordinates)
+        .setHTML(htmlString)
+        .addTo(map);
     });
 
-    console.log('Features:', features);
+    // change cursor to pointer on launchpad hover
+    map.on('mouseenter', 'symbols', function () {
+      map.getCanvas().style.cursor = 'pointer';
+    });
 
-    // send features into map.on('load') function
-    callback(features);
-  })
-  .catch(function (error) {
-    console.log(error);
+    // change cursor to default when mouse leaves launchpad
+    map.on('mouseleave', 'symbols', function () {
+      map.getCanvas().style.cursor = '';
+    });
+  });
+});
+
+
+//////////////////////
+// HELPER FUNCTIONS //
+//////////////////////
+
+
+// format launch pad objects to be used by map
+function createFeatureList(launchPads) {
+  return launchPads.map(function(pad) {
+    return {
+      type: 'Feature',
+      properties: {
+        id: pad.id,
+        name: pad.name,
+        agencies: pad.agencies
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          pad.longitude,
+          pad.latitude
+        ]
+      }
+    };
   });
 };
 
 
-// run anonymous function when map loads
-map.on('load', function () {
-  // RUN FUNCTION THAT CREATES A "features" ARRAY OF "FEATURE" OBJECTS (line 25)
-  createFeatures(createLayer);
+// add feature list to 'symbols' map layer
+function addFeaturesToMap(featureList) {
+  // concat old feature list with new feature list
+  var newFeatureList = map.getSource('symbols')._data.features.concat(featureList);
+  // update symbols layer with new feature list
+  map.getSource('symbols').setData({
+    type: 'FeatureCollection',
+    features: newFeatureList
+  });
+};
 
-  // Add a symbol layer.
-  // map.addLayer({
-  //     "id": "symbols",
-  //     "type": "symbol",
-  //     "source": {
-  //         "type": "geojson",
-  //         "data": {
-  //             "type": "FeatureCollection",
-  //             "features": [
-  //                 {
-  //                     "type": "Feature",
-  //                     "properties": {},
-  //                     "geometry": {
-  //                         "type": "Point",
-  //                         "coordinates": [
-  //                             -91.395263671875,
-  //                             -0.9145729757782163
 
-  //                         ]
-  //                     }
-  //                 },
-  //                 {
-  //                     "type": "Feature",
-  //                     "properties": {},
-  //                     "geometry": {
-  //                         "type": "Point",
-  //                         "coordinates": [
-  //                             -90.32958984375,
-  //                             -0.6344474832838974
-  //                         ]
-  //                     }
-  //                 },
-  //                 {
-  //                     "type": "Feature",
-  //                     "properties": {},
-  //                     "geometry": {
-  //                         "type": "Point",
-  //                         "coordinates": [
-  //                             -91.34033203125,
-  //                             0.01647949196029245
-  //                         ]
-  //                     }
-  //                 }
-  //             ]
-  //         }
-  //     },
-  //     "layout": {
-  //         "icon-image": "rocket-15",
-  //         "icon-allow-overlap": true
-  //     }
-  // });
+// returns 30 launch pad objects at a time
+// offset = number (0, 30, 60, 90, 120, etc)
+function addLaunchPadsToMap(offset) {
+  offset = offset || 0;
+  // get launch pads
+  return axios.get('https://launchlibrary.net/1.2/pad?mode=verbose&offset=' + offset)
+    .then(function(response) {
+      return response.data.pads;
+    })
+    .then(function(launchPads) {
+      // create feature list
+      if (launchPads.length) {
+        return createFeatureList(launchPads);
+      }
+    })
+    .then(function(featureList) {
+      var nextPage = false;
+      // add features to map
+      if (featureList) {
+        addFeaturesToMap(featureList);
+        nextPage = true;
+      }
+      return nextPage;
+    })
+    .then(function(nextPage) {
+      // add next page of results to map
+      if (nextPage) {
+        addLaunchPadsToMap(offset + 30);
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+};
 
-  // // Center the map on the coordinates of any clicked symbol from the 'symbols' layer.
-  // map.on('click', 'symbols', function (e) {
-  //     map.flyTo({center: e.features[0].geometry.coordinates, zoom: 8});
-  //     new mapboxgl.Popup()
-  //       .setLngLat(e.features[0].geometry.coordinates)
-  //       .setHTML('Hello World!')
-  //       .addTo(map);
-  // });
 
-  // // Change the cursor to a pointer when the it enters a feature in the 'symbols' layer.
-  // map.on('mouseenter', 'symbols', function () {
-  //     map.getCanvas().style.cursor = 'pointer';
-  // });
-
-  // // Change it back to a pointer when it leaves.
-  // map.on('mouseleave', 'symbols', function () {
-  //     map.getCanvas().style.cursor = '';
-  // });
-});
+// get individual rocket info
+function getRockets(offset, rocketList) {
+  offset = offset || 0;
+  rocketList = rocketList || [];
+  return axios.get('https://launchlibrary.net/1.2/rocket?mode=list&offset=' + offset)
+    .then(function(response) {
+      if (response.data.rockets.length) {
+        var newRocketList = rocketList.concat(response.data.rockets);
+        return getRockets(offset + 30, newRocketList);
+      } else {
+        return rocketList;
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+};
